510d92c47364635337876d3a64bf7cc1bf45fc98
Multi <88001300+MultiViSa@users.noreply.github.com>
2024-01-16 15:24:25 +0100

    Regiter activate via click

---
+++ script.js
@@ -0,0 +1,7 @@
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-Link');
+
+registerLink.addEventListener('click', ()=> {
+    wrapper.classList.add('active');
+});
